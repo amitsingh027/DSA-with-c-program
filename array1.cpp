8ebf780df8e97799ecaa275debf5461cf991c746
amitsingh027 <amitsingh.ghz27@gmail.com>
2024-09-10 20:37:23 +0530

    add linear search

--- array1.cpp
+++ array1.cpp
@@ -177,6 +177,66 @@ int main()
 
 
 
+/*
+//linear search
+#include <iostream>
+using namespace std;
+
+int search(int arr[], 
+		int n, int x)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		if (arr[i] == x)
+			return i;
+	return -1;
+}
+int main(void)
+{
+	int arr[] = {2, 3, 4, 10, 40};
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	int result = search(arr, n, x);
+	(result == -1) ? 
+	cout << "Element is not present in array" : 
+	cout << "Element is present at index " << 
+			result;
+	return 0;
+}
+*/
+
+
+
+
+
+/*
+//find the missing number in an array
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int N) {
+
+    int sum = (N * (N + 1)) / 2;
+
+    int s2 = 0;
+    for (int i = 0; i < N - 1; i++) {
+        s2 += a[i];
+    }
+
+    int missingNum = sum - s2;
+    return missingNum;
+}
+
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, N);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
+*/
 
 
 
@@ -184,3 +244,4 @@ int main()
 
 
 
+//find maximum consecutive ones in the array
